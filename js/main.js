0b1c824e3d974a602f0e51e5018469a6eb1846fa
Lorran David <lorrantd1@gmail.com>
2018-06-05 08:31:55 -0300

    Started UI events handling

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ function createList(UI) {
         var id = btoa('titulo do post');
         var todo = { id: id, description: data.description, status: 00 };
         todos[id] = todo;
+        UI.addToList(id, todo);
     }
 
     /**
@@ -61,5 +62,77 @@ function createList(UI) {
 
 // UI
 function createUI() {
+    var TEMPLATE = `
+        <h2>Lorem Ipsum1</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut quidem ratione hic nam libero asperiores inventore pariatur unde quisquam aliquam! Eligendi ad vel hic minima et quas repellendus ex! Iste?</p>
+        <input type="checkbox" name="done" data-js-action="done" value="11"><label for="done">Finalizar</label>
+        <a href="#" data-js-action="remove">Remover</a>
+    `;
+    var $todoElements = [];
+    var $todosContainer = document.querySelector('#todos');
+    var $btnAddNew = document.querySelector('[data-js-action="addNewTodo"]');
+    var $inputTitle = document.querySelector('[data-js-id="title"]');
+    var $inputDescription = document.querySelector('[data-js-id="description"]');
 
+    /**
+     * Handle form event
+     * @param {*} e
+     */
+    function handleNewTodo(e) {
+        e.preventDefault();
+        App.add({});
+    }
+
+    /**
+     * Initialize events
+     */
+    function init() {
+        $btnAddNew.addEventListener('click', handleNewTodo);
+    }
+
+    /**
+     * Render template
+     * @param {*} data
+     */
+    function render(data) {
+        var { title, description } = data;
+        return `
+            <h2>${title}</h2>
+            <p>${description}</p>
+            <input type="checkbox" name="done" data-js-action="done" value="11"><label for="done">Finalizar</label>
+            <a href="#" data-js-action="remove">Remover</a>
+        `;
+    }
+
+    /**
+     * Add new todo to the UI
+     * @param {*} id
+     * @param {*} data
+     */
+    function addToList(id, data) {
+        var fragment = document.createDocumentFragment();
+        var wrapper = document.createElement('div');
+        wrapper.classList.add = 'todo';
+        wrapper.setAttribute('data-js-id', '123');
+        wrapper.innerHTML = render({ title: 'Novo', description: 'Desc' });
+        $todosContainer.appendChild(fragment.appendChild(wrapper));
+    };
+
+    /**
+     * Public API
+     */
+    var publicAPI = {
+        init,
+        addToList
+    };
+
+    return publicAPI;
 }
+
+/**
+ * Initialize
+ */
+var UI = createUI();
+UI.init();
+
+var App = createList(UI);
