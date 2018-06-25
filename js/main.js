fe068c74e74adc35d6b452594132b7ac6c922939
Lorran David <lorrantd1@gmail.com>
2018-06-25 11:06:59 -0300

    Adding new functionality

--- js/main.js
+++ js/main.js
@@ -1,172 +1,99 @@
-(function (document, global) {
-  /**
-   * Helpers
-   */
-  var Helpers = {
-    generateId: function () {
-      return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, function (c) {
-        return (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16);
-      });
-    },
-    validation: {
-      rules: {
-        required: function (data) {
-          return data.length > 0 && data !== ' ';
-        },
-        min: function (data, length) {
-          return data.length > length;
-        },
-        max: function (data, length) {
-          return data.length < length;
-        },
-        type: function (data, type) {
-          return typeof data === type;
+(function(document, global) {
+    /**
+     * Helpers
+     */
+    var Helpers = {
+        generateId: function() {
+            return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, function(c) {
+                return (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16);
+            });
         }
-      }
-    }
-  };
-
-  function $(selector) {
-    return document.querySelectorAll(selector);
-  };
-
-  // TODO
-  function createList(UI) {
-    var todos = {};
-
-    /**
-     * Create todo
-     * @param {*} data
-     */
-    function add(data) {
-      var id = Helpers.generateId();
-      var todo = {
-        id: id,
-        title: data.title,
-        description: data.description,
-        status: 00
-      };
-      todos[id] = todo;
-      UI.addToList(id, todo);
-    }
-
-    /**
-     * Edit todo
-     * @param {*} id
-     */
-    function edit(id) {
-
-    }
-
-    /**
-     * Remove todo
-     * @param {*} id
-     */
-    function remove(id) {
-      if (todos[id] === undefined) {
-        return false;
-      }
-      delete todos[id];
-      return true;
-    }
-
-    /**
-     * Mark todo as done/not done
-     * @param {*} id
-     * @param {*} status
-     */
-    function mark(id, status) {
-      if (todos[id] === undefined) {
-        return false;
-      }
-      todos[id].status = status;
-    }
-
-    /**
-     * Move todo up or down on the list
-     * @param {*} direction
-     */
-    function move(direction) {
-
-    }
-
-    /**
-     * Public API
-     */
-    var publicAPI = {
-      add,
-      edit,
-      remove,
-      mark,
-      move
     };
 
-    return publicAPI;
-  }
-
-
-  // UI
-  function createUI() {
-    var $todoElements = [];
-    var $todosContainer = $('#todos')[0];
-    var $btnAddNew = $('[data-js-action="addNewTodo"]')[0];
-    var $inputTitle = $('[data-js-id="title"]')[0];
-    var $inputDescription = $('[data-js-id="description"]')[0];
-
     /**
-     * Handle form event
-     * @param {*} e
+     * UI Factory
      */
-    function handleNewTodo(e) {
-      e.preventDefault();
-      App.add(getFormData());
-    }
+    function createUI() {
+        var $todoElements = [];
+        var $todosContainer;
+        var $btnAddNew;
+        var $inputTitle;
+        var $inputDescription;
 
-    /**
-     * Handle form event
-     * @param {*} e
-     */
-    function handleRemoveTodo(e) {
-      e.preventDefault();
-      var todo = e.target.parentNode;
-      var id = todo.getAttribute('data-js-id');
+        /**
+         * Handle form event
+         * @param {*} e
+         */
+        function handleNewTodo(e) {
+            e.preventDefault();
+            App.add(getFormData());
+        }
 
-      if (App.remove(id)) {
-        $todoElements = $todoElements.filter(function (todo) {
-          return todo.getAttribute('data-js-id') !== id;
-        });
-        $todosContainer.removeChild(todo);
-      }
-    }
+        /**
+         * Handle form event
+         * @param {*} e
+         */
+        function handleRemoveTodo(e) {
+            e.preventDefault();
+            var todo = e.target.parentNode;
+            var id = todo.getAttribute('data-js-id');
 
-    function handleDoneTodo(e) {
-      var $checkbox = e.target;
-      var id = $checkbox.parentNode.parentNode.getAttribute('data-js-id');
+            if (App.remove(id)) {
+                $todoElements = $todoElements.filter(function(todo) {
+                    return todo.getAttribute('data-js-id') !== id;
+                });
+                $todosContainer.removeChild(todo);
+            }
+        }
 
-      if ($checkbox.checked) {
-        $checkbox.parentNode.parentNode.classList.add('todo--done');
-        App.mark(id, '11');
-      } else {
-        $checkbox.parentNode.parentNode.classList.remove('todo--done');
-        App.mark(id, '00');
-      }
-    }
+        function handleDoneTodo(e) {
+            var $checkbox = e.target;
+            var id = $checkbox.parentNode.parentNode.getAttribute('data-js-id');
 
-    /**
-     * Initialize events
-     */
-    function init() {
-      $btnAddNew.addEventListener('click', handleNewTodo);
-    }
+            if ($checkbox.checked) {
+                $checkbox.parentNode.parentNode.classList.add('todo--done');
+                App.mark(id, '11');
+            } else {
+                $checkbox.parentNode.parentNode.classList.remove('todo--done');
+                App.mark(id, '00');
+            }
+        }
 
-    /**
-     * Render todo
-     * @param {*} id
-     * @param {*} title
-     * @param {*} description
-     */
-    function render(id, title, description) {
-      var template = `
+        /**
+         * Easy-to-use dom selector
+         * @param {*} selector
+         */
+        function $(selector) {
+            var $elems = document.querySelectorAll(selector);
+
+            if ($elems.length > 1) {
+                return $elems;
+            }
+            return $elems[0];
+        };
+
+        /**
+         * Initialize UI
+         */
+        function init() {
+            $todosContainer = $('#todo');
+            $doingContainer = $('#doing');
+            $doneContainer = $('#done');
+            $btnAddNew = $('[data-js-action="addNewTodo"]');
+            $inputTitle = $('[data-js-id="title"]');
+            $inputDescription = $('[data-js-id="description"]');
+            $inputPriority = $('[data-js-id="priority"]');
+            $btnAddNew.addEventListener('click', handleNewTodo);
+        }
+
+        /**
+         * Render todo
+         * @param {*} id
+         * @param {*} title
+         * @param {*} description
+         */
+        function render(id, title, description) {
+            var template = `
             <div class="todo-cb">
                 <input type="checkbox" class="todo-cb__input" name="done" data-js-action="done" value="11">
             </div>
@@ -178,77 +105,157 @@
             <p></p>
             <a href="#" data-js-action="remove">Remover</a>
         `;
-      var fragment = document.createDocumentFragment();
-      var wrapper = document.createElement('div');
-      wrapper.classList.add('todo');
-      wrapper.setAttribute('data-js-id', id);
-      wrapper.innerHTML = template;
-      fragment.appendChild(wrapper)
-      $todosContainer.insertBefore(fragment, $todosContainer.firstChild);
+            var fragment = document.createDocumentFragment();
+            var wrapper = document.createElement('div');
+            wrapper.classList.add('todo');
+            wrapper.setAttribute('data-js-id', id);
+            wrapper.innerHTML = template;
+            fragment.appendChild(wrapper)
+            $todosContainer.firstElementChild.after(fragment);
 
-      /**
-       * Events
-       */
-      var todo = $('.todo[data-js-id="' + id + '"]')[0];
-      var $btnRemove = todo.querySelector('[data-js-action="remove"]');
-      var $checkbox = todo.querySelector('[data-js-action="done"]');
-      $btnRemove.addEventListener('click', handleRemoveTodo);
-      $checkbox.addEventListener('change', handleDoneTodo);
-      return wrapper;
+            /**
+             * Events
+             */
+            var todo = $('.todo[data-js-id="' + id + '"]');
+            var $btnRemove = todo.querySelector('[data-js-action="remove"]');
+            var $checkbox = todo.querySelector('[data-js-action="done"]');
+            $btnRemove.addEventListener('click', handleRemoveTodo);
+            $checkbox.addEventListener('change', handleDoneTodo);
+            return wrapper;
+        }
+
+        /**
+         * Add todo to list
+         * @param {*} id
+         * @param {*} data
+         */
+        function addToList(id, data) {
+            var todo = render(id, data.title, data.description);
+            $todoElements.unshift(todo);
+        }
+
+        /**
+         * Get form data
+         */
+        function getFormData() {
+            var title = $inputTitle.value;
+            var description = $inputDescription.value;
+            var priority = $inputPriority.value;
+            $inputTitle.value = '';
+            $inputDescription.value = '';
+
+            return {
+                title,
+                description,
+                priority
+            };
+        }
+
+        /**
+         * Public API
+         */
+        var publicAPI = {
+            init,
+            addToList
+        };
+
+        return publicAPI;
     }
 
     /**
-     * Add todo to list
-     * @param {*} id
-     * @param {*} data
+     * TODO List Factory
+     * @param {*} UI
      */
-    function addToList(id, data) {
-      var todo = render(id, data.title, data.description);
-      $todoElements.unshift(todo);
+    function createList(UI) {
+        var todos = {};
+
+        /**
+         * Create todo
+         * @param {*} data
+         */
+        function add(data) {
+            var id = Helpers.generateId();
+            var todo = {
+                id: id,
+                title: data.title,
+                description: data.description,
+                status: 00
+            };
+            todos[id] = todo;
+            UI.addToList(id, todo);
+        }
+
+        /**
+         * Edit todo
+         * @param {*} id
+         */
+        function edit(id) {
+
+        }
+
+        /**
+         * Remove todo
+         * @param {*} id
+         */
+        function remove(id) {
+            if (todos[id] === undefined) {
+                return false;
+            }
+            delete todos[id];
+            return true;
+        }
+
+        /**
+         * Mark todo as done/not done
+         * @param {*} id
+         * @param {*} status
+         */
+        function mark(id, status) {
+            if (todos[id] === undefined) {
+                return false;
+            }
+            todos[id].status = status;
+        }
+
+        /**
+         * Move todo up or down on the list
+         * @param {*} direction
+         */
+        function move(direction) {
+
+        }
+
+        /**
+         * Public API
+         */
+        var publicAPI = {
+            add,
+            edit,
+            remove,
+            mark,
+            move
+        };
+
+        return publicAPI;
     }
 
     /**
-     * Get form data
+     * Initialize
      */
-    function getFormData() {
-      var title = $inputTitle.value;
-      var description = $inputDescription.value;
-      $inputTitle.value = '';
-      $inputDescription.value = '';
-      return {
-        title,
-        description
-      };
-    }
+    var UI = createUI();
+    UI.init();
+
+    var App = createList(UI);
 
     /**
-     * Public API
+     * Hardcoding some initial data
      */
-    var publicAPI = {
-      init,
-      addToList
-    };
-
-    return publicAPI;
-  }
-
-  /**
-   * Initialize
-   */
-  var UI = createUI();
-  UI.init();
-
-  var App = createList(UI);
-
-  /**
-   * Hardcoding some initial data
-   */
-  App.add({
-    title: 'Lorem Ipsum',
-    description: 'Lorem ipsum dolor dictum sociosqu aenea.'
-  });
-  App.add({
-    title: 'Vehicula varius blandit',
-    description: 'Pretium porttitor turpis sit nulla mi erat mattis lorem.'
-  });
+    // App.add({
+    //   title: 'Lorem Ipsum',
+    //   description: 'Lorem ipsum dolor dictum sociosqu aenea.'
+    // });
+    // App.add({
+    //   title: 'Vehicula varius blandit',
+    //   description: 'Pretium porttitor turpis sit nulla mi erat mattis lorem.'
+    // });
 })(document, window);
